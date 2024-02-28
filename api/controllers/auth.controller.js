9cd2414864567656abbe995c0c2e828f6ba8a202
kenanhomsi <kinanhomsi0959@gmail.com>
2024-02-28 14:45:37 +0300

    create sigin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken'
+
 export const signup=async (req,res,next)=>{
    const {username ,email ,password}=req.body;
    
@@ -23,4 +25,27 @@ export const signup=async (req,res,next)=>{
      next(err);
    }
 
+}
+
+export const signin = async (req,res,next)=>{
+  const {email ,password}= req.body;
+
+  if(!email || !password || email=='' || password==''){
+    return next(errorHandler(400,'All fields are required '))
+  }
+  try{
+    const ValidUser= await User.findOne({email});
+    if(!ValidUser){
+     return next(errorHandler(404,'User not found'))
+    }
+    const ValidPassword= bcryptjs.compareSync(password,ValidUser.password);
+    if(!ValidPassword){
+       return next(errorHandler(400,'invalid password'));
+    }
+    const token= jwt.sign({id:ValidUser._id} ,process.env.JWT_SECRET);
+    const {password:pass , ...rest}=ValidUser._doc;
+    res.status(200).cookie('access-token',token,{httpOnly:true}).json(rest);
+  }catch(err){
+    next(err);
+  }
 }
