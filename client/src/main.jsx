f873a94d20cc84515bcb2351d7920b78559703f5
kenanhomsi <kinanhomsi0959@gmail.com>
2024-02-28 16:55:26 +0300

    add redux toolit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
+import { store } from '../redux/store'
+import { Provider } from 'react-redux'
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Provider  store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 )
