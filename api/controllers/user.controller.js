c40b5d4819c9dbc3391c089d3fe641461ef9a925
kenanhomsi <kinanhomsi0959@gmail.com>
2024-03-03 19:58:49 +0300

    add delete route user api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -46,3 +46,14 @@ export const updateUser= async(req,res,next)=>{
   }
 
 }
+export const deleteUser= async(req,res,next)=>{
+  if(req.user.id !=req.params.userId){
+    return next(errorHandler(403,'your are not allowed to update this user'));
+  }
+  try{
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(200).json('User has been deleted');
+  }catch(err){
+    next(err)
+  }
+}
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import {test , updateUser } from '../controllers/user.controller.js'
+import {test , updateUser ,deleteUser} from '../controllers/user.controller.js'
 import { verifyToken } from "../utils/verifyUser.js";
 const routes=express.Router();
 
 
 routes.get('/test',test);
 routes.put('/update/:userId', verifyToken  ,updateUser);
+routes.delete('/delete/:userId', verifyToken  ,deleteUser);
   
   export default routes;
