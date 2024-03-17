47896be071093a6be9e92b59f44b8db37f871ece
kenanhomsi <kinanhomsi0959@gmail.com>
2024-03-17 18:29:43 +0300

    update option value in search page

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -110,7 +110,7 @@ export default function Search() {
                             <option value="uncategorized">Uncategorized</option>
                             <option value="reactjs">React.js</option>
                             <option value="javascript">JavaScript</option>
-                            <option value="next">Next.js</option>
+                            <option value="nextjs">Next.js</option>
                     </Select>
                 </div>
                 <Button  type='submit' gradientDuoTone='purpleToPink' outline> Apply Filter</Button>
